9055461390727856e72d16f1c6e98b62093d8392
Anson Ho <mrho888@gmail.com>
2019-04-14 16:51:32 +0800

    Update User-Agent

--- cu.py
+++ cu.py
@@ -24,7 +24,7 @@ WHITELIST = set([])
 BLACKLIST = set([])
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                          'AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/65.0.3325.181 Safari/537.36'}
+                         'Chrome/73.0.3683.103 Safari/537.36'}
 RETRY_CNT = 10
 TIMEOUT = 60  # 60 sec
 PIAZZA_MAIN_URL = 'https://piazza.com'
